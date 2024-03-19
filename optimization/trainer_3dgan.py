f3c1fd32540a029713de7bb9df1e1e612a8ed2f5
ge26yim <ce.winkler@tum.de>
2024-03-19 15:08:45 +0100

    change loss

--- optimization/trainer_3dgan.py
+++ optimization/trainer_3dgan.py
@@ -104,40 +104,43 @@ def trainer(args, train_loader, valid_loader, generator, discriminator,
             score_fake = discriminator(gen_x_for.detach())
 
             # define tensors
-            real_score = Variable(torch.FloatTensor(args.bsz, 1).fill_(1)).to(args.device)
-            fake_score = Variable(torch.FloatTensor(args.bsz, 1).fill_(0)).to(args.device)
+            real_labels = Variable(torch.FloatTensor(args.bsz, 1).fill_(1)).to(args.device)
+            fake_labels = Variable(torch.FloatTensor(args.bsz, 1).fill_(0)).to(args.device)
 
-            # wasserstein gradient penalty loss
-            loss_d = torch.mean(score_fake) - torch.mean(score_real)
+            # # wasserstein gradient penalty loss
+            # loss_d = torch.mean(score_fake) - torch.mean(score_real)
+            #
+            # # gradient penalty
+            # lam = 10
+            # alpha = torch.randn(args.bsz, 1)
+            # alpha = alpha.expand(args.bsz, x[:,:,2:,:,:][0].nelement()).contiguous().view(args.bsz, x.size(1), x[:,:,2:,:,:].size(2), x.size(3), x.size(4)).to(args.device)
+            # interpolates = alpha*x[:,:,2:,:,:]+((1-alpha)*gen_x_for).to(args.device)
+            # interpolates = Variable(interpolates, requires_grad=True).to(args.device)
+            # interpolates_score = discriminator(interpolates)
+            # gradients = torch.autograd.grad(outputs=interpolates_score, inputs=interpolates,
+            #                                 grad_outputs=torch.ones(interpolates_score.size()).cuda(),
+            #                                 create_graph=True, retain_graph=True, only_inputs=True)[0]
 
-            # gradient penalty
-            lam = 10
-            alpha = torch.randn(args.bsz, 1)
-            alpha = alpha.expand(args.bsz, x[:,:,2:,:,:][0].nelement()).contiguous().view(args.bsz, x.size(1), x[:,:,2:,:,:].size(2), x.size(3), x.size(4)).to(args.device)
-            interpolates = alpha*x[:,:,2:,:,:]+((1-alpha)*gen_x_for).to(args.device)
-            interpolates = Variable(interpolates, requires_grad=True).to(args.device)
-            interpolates_score = discriminator(interpolates)
-            gradients = torch.autograd.grad(outputs=interpolates_score, inputs=interpolates,
-                                            grad_outputs=torch.ones(interpolates_score.size()).cuda(),
-                                            create_graph=True, retain_graph=True, only_inputs=True)[0]
+            # # compute gradients
+            # gradients = gradients.view(gradients.size(0), -1)
+            # gradient_penalty = ((gradients.norm(2, dim=1)-1)**2).mean()
+            # loss_d = loss_d+lam*gradient_penalty
 
-            # compute gradients
-            gradients = gradients.view(gradients.size(0), -1)
-            gradient_penalty = ((gradients.norm(2, dim=1)-1)**2).mean()
-            loss_d = loss_d+lam*gradient_penalty
+            # eps = 0.001
+            # eps_penalty = torch.mean((real_score-0)**2)
+            # loss_d = loss_d+eps_penalty*eps
 
-            eps = 0.001
-            eps_penalty = torch.mean((real_score-0)**2)
-            loss_d = loss_d+eps_penalty*eps
+            real_loss = criterion(score_real, real_labels)
+            fake_loss = criterion(score_fake, fake_labels)
+            loss_d = real_loss + fake_loss
             loss_d.backward()
 
             # update discriminator
             optimizerD.step()
 
-            # get discriminator output
-            fake_score = discriminator(gen_x_for)
-            loss_g = -torch.mean(fake_score)
-
+            # get discriminator output for fake image
+            fake_score_gen = discriminator(gen_x_for)
+            loss_g = criterion(fake_score_gen, fake_labels)
             loss_g.backward()
             optimizerG.step()
 
